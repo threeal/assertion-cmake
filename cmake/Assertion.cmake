59b7a7c31d4cb112f978f6042a7f58f5eac7ebb0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-03 21:39:07 +0700

    feat: add variable defined assertion functions

--- cmake/Assertion.cmake
+++ cmake/Assertion.cmake
@@ -22,3 +22,23 @@ function(assert_false CONDITION)
     message(FATAL_ERROR "expected the condition to be false")
   endif()
 endfunction()
+
+# Asserts whether the given variable is defined.
+#
+# Arguments:
+#   - VARIABLE: The variable to assert.
+function(assert_defined VARIABLE)
+  if(NOT DEFINED "${VARIABLE}")
+    message(FATAL_ERROR "expected variable '${VARIABLE}' to be defined")
+  endif()
+endfunction()
+
+# Asserts whether the given variable is not defined.
+#
+# Arguments:
+#   - VARIABLE: The variable to assert.
+function(assert_not_defined VARIABLE)
+  if(DEFINED "${VARIABLE}")
+    message(FATAL_ERROR "expected variable '${VARIABLE}' not to be defined")
+  endif()
+endfunction()
