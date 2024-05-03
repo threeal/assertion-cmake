dea7c31c066d5534137d63e3d85f4df474c21c20
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-03 21:39:52 +0700

    test: add test for testing variable defined assertion functions

--- test/cmake/AssertionTest.cmake
+++ test/cmake/AssertionTest.cmake
@@ -10,6 +10,15 @@ function(test_assert_a_false_condition)
   assert_false(FALSE)
 endfunction()
 
+function(test_assert_a_defined_variable)
+  set(SOME_VARIABLE "some value")
+  assert_defined(SOME_VARIABLE)
+endfunction()
+
+function(test_assert_an_undefined_variable)
+  assert_not_defined(SOME_VARIABLE)
+endfunction()
+
 if(NOT DEFINED TEST_COMMAND)
   message(FATAL_ERROR "The 'TEST_COMMAND' variable should be defined")
 elseif(NOT COMMAND test_${TEST_COMMAND})
