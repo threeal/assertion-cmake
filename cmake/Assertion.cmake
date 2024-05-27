bd84d8060b82c1809a2e175c9278390f170c5494
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-27 22:34:42 +0700

    feat: merge message assertion functions to `assertion` function (#47)

--- cmake/Assertion.cmake
+++ cmake/Assertion.cmake
@@ -10,60 +10,86 @@ include_guard(GLOBAL)
 #
 # Refer to the documentation of the 'if' function for supported conditions to
 # perform the assertion.
+#
+# Aside from the assertions specified in the documentation of the 'if' function,
+# this function also supports mocked message assertion by calling
+# 'assert(MESSAGE <MODE> <EXPECTED_MESSAGE>)', where 'MODE' is the message mode
+# and 'EXPECTED_MESSAGE' is the expected message.
 function(assert)
   list(LENGTH ARGN ARGUMENTS_LENGTH)
   if(ARGUMENTS_LENGTH GREATER 0)
     set(ARGUMENTS ${ARGN})
 
-    # Determines whether the given arguments start with 'NOT'.
-    list(GET ARGUMENTS 0 ARGUMENTS_0)
-    if(ARGUMENTS_0 STREQUAL NOT)
-      list(REMOVE_AT ARGUMENTS 0)
-      set(BOOLEAN_WORD " false")
-      set(NOT_WORD " not")
+    list(GET ARGUMENTS 0 ASSERTION)
+    if(ASSERTION STREQUAL MESSAGE)
+      list(LENGTH ARGUMENTS ARGUMENTS_LENGTH)
+      if(ARGUMENTS_LENGTH LESS 3)
+        message(FATAL_ERROR "usage: assert(MESSAGE <MODE> <EXPECTED_MESSAGE>)")
+      endif()
+      list(GET ARGUMENTS 1 MODE)
+      list(GET ARGUMENTS 2 EXPECTED_MESSAGE)
+
+      list(POP_FRONT ${MODE}_MESSAGES MESSAGE)
+      if(NOT MESSAGE STREQUAL EXPECTED_MESSAGE)
+        string(TOLOWER "${MODE}" MODE)
+        string(REPLACE "_" " " MODE "${MODE}")
+        message(FATAL_ERROR "expected ${MODE} message '${MESSAGE}' to be equal to '${EXPECTED_MESSAGE}'")
+      endif()
+
+      if(DEFINED ${MODE}_MESSAGES)
+        set(${MODE}_MESSAGES "${${MODE}_MESSAGES}" PARENT_SCOPE)
+      endif()
     else()
-      set(ARGUMENT_NOT NOT)
-      set(BOOLEAN_WORD " true")
-    endif()
-
-    list(LENGTH ARGUMENTS ARGUMENTS_LENGTH)
-    if(ARGUMENTS_LENGTH EQUAL 2)
-      list(GET ARGUMENTS 0 OPERATOR)
-      list(GET ARGUMENTS 1 VALUE)
-
-      if(OPERATOR STREQUAL DEFINED)
-        set(MESSAGE "expected variable '${VALUE}'${NOT_WORD} to be defined")
-      elseif(OPERATOR STREQUAL EXISTS)
-        set(MESSAGE "expected path '${VALUE}'${NOT_WORD} to exist")
-      elseif(OPERATOR STREQUAL IS_DIRECTORY)
-        set(MESSAGE "expected path '${VALUE}'${NOT_WORD} to be a directory")
-      endif()
-    elseif(ARGUMENTS_LENGTH EQUAL 3)
-      list(GET ARGUMENTS 0 LEFT_VALUE)
-      list(GET ARGUMENTS 1 OPERATOR)
-      list(GET ARGUMENTS 2 RIGHT_VALUE)
-
-      if(OPERATOR STREQUAL MATCHES)
-        if(DEFINED "${LEFT_VALUE}")
-          set(LEFT_VALUE "${${LEFT_VALUE}}")
-        endif()
-        set(MESSAGE "expected string '${LEFT_VALUE}'${NOT_WORD} to match '${RIGHT_VALUE}'")
-      elseif(OPERATOR STREQUAL STREQUAL)
-        if(DEFINED "${LEFT_VALUE}")
-          set(LEFT_VALUE "${${LEFT_VALUE}}")
-        endif()
-        if(DEFINED "${RIGHT_VALUE}")
-          set(RIGHT_VALUE "${${RIGHT_VALUE}}")
-        endif()
-        set(MESSAGE "expected string '${LEFT_VALUE}'${NOT_WORD} to be equal to '${RIGHT_VALUE}'")
-      endif()
-    endif()
-
-    if(${ARGUMENT_NOT} ${ARGUMENTS})
-      if(DEFINED MESSAGE)
-        message(FATAL_ERROR "${MESSAGE}")
+      # Determines whether the given arguments start with 'NOT'.
+      list(GET ARGUMENTS 0 ARGUMENTS_0)
+      if(ARGUMENTS_0 STREQUAL NOT)
+        list(REMOVE_AT ARGUMENTS 0)
+        set(BOOLEAN_WORD " false")
+        set(NOT_WORD " not")
       else()
-        message(FATAL_ERROR "expected '${ARGUMENTS}' to resolve to${BOOLEAN_WORD}")
+        set(ARGUMENT_NOT NOT)
+        set(BOOLEAN_WORD " true")
+      endif()
+
+      list(LENGTH ARGUMENTS ARGUMENTS_LENGTH)
+      if(ARGUMENTS_LENGTH EQUAL 2)
+        list(GET ARGUMENTS 0 OPERATOR)
+        list(GET ARGUMENTS 1 VALUE)
+
+        if(OPERATOR STREQUAL DEFINED)
+          set(MESSAGE "expected variable '${VALUE}'${NOT_WORD} to be defined")
+        elseif(OPERATOR STREQUAL EXISTS)
+          set(MESSAGE "expected path '${VALUE}'${NOT_WORD} to exist")
+        elseif(OPERATOR STREQUAL IS_DIRECTORY)
+          set(MESSAGE "expected path '${VALUE}'${NOT_WORD} to be a directory")
+        endif()
+      elseif(ARGUMENTS_LENGTH EQUAL 3)
+        list(GET ARGUMENTS 0 LEFT_VALUE)
+        list(GET ARGUMENTS 1 OPERATOR)
+        list(GET ARGUMENTS 2 RIGHT_VALUE)
+
+        if(OPERATOR STREQUAL MATCHES)
+          if(DEFINED "${LEFT_VALUE}")
+            set(LEFT_VALUE "${${LEFT_VALUE}}")
+          endif()
+          set(MESSAGE "expected string '${LEFT_VALUE}'${NOT_WORD} to match '${RIGHT_VALUE}'")
+        elseif(OPERATOR STREQUAL STREQUAL)
+          if(DEFINED "${LEFT_VALUE}")
+            set(LEFT_VALUE "${${LEFT_VALUE}}")
+          endif()
+          if(DEFINED "${RIGHT_VALUE}")
+            set(RIGHT_VALUE "${${RIGHT_VALUE}}")
+          endif()
+          set(MESSAGE "expected string '${LEFT_VALUE}'${NOT_WORD} to be equal to '${RIGHT_VALUE}'")
+        endif()
+      endif()
+
+      if(${ARGUMENT_NOT} ${ARGUMENTS})
+        if(DEFINED MESSAGE)
+          message(FATAL_ERROR "${MESSAGE}")
+        else()
+          message(FATAL_ERROR "expected '${ARGUMENTS}' to resolve to${BOOLEAN_WORD}")
+        endif()
       endif()
     endif()
   endif()
@@ -104,31 +130,6 @@ endfunction()
 # to the original behavior.
 function(end_mock_message)
   set_property(GLOBAL PROPERTY message_mocked OFF)
-endfunction()
-
-# Asserts whether the 'message' function was called with the expected
-# arguments.
-#
-# This function asserts whether a message with the specified mode was called
-# with the expected message content.
-#
-# This function can only assert calls to the mocked 'message' function, which
-# is enabled by calling the 'mock_message' function.
-#
-# Arguments:
-#   - MODE: The message mode.
-#   - EXPECTED_MESSAGE: The expected message content.
-function(assert_message MODE EXPECTED_MESSAGE)
-  list(POP_FRONT ${MODE}_MESSAGES MESSAGE)
-  if(NOT MESSAGE STREQUAL EXPECTED_MESSAGE)
-    string(TOLOWER "${MODE}" MODE)
-    string(REPLACE "_" " " MODE "${MODE}")
-    message(FATAL_ERROR "expected ${MODE} message '${MESSAGE}' to be equal to '${EXPECTED_MESSAGE}'")
-  endif()
-
-  if(DEFINED ${MODE}_MESSAGES)
-    set(${MODE}_MESSAGES "${${MODE}_MESSAGES}" PARENT_SCOPE)
-  endif()
 endfunction()
 
 # Asserts whether the given command successfully executes a process.
