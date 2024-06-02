d64f7a2f75471523b3cf5075e1cb8c0bbb997093
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-02 15:10:24 +0700

    refactor: modify mock message function to be internal (#76)

--- test/cmake/AssertionTest.cmake
+++ test/cmake/AssertionTest.cmake
@@ -188,27 +188,4 @@ function("Process execution assertions")
     MESSAGE "${EXPECTED_MESSAGE}")
 endfunction()
 
-function("Mock message")
-  mock_message()
-    message(WARNING "some warning message")
-    message(WARNING "some other warning message")
-
-    mock_message()
-      message(ERROR "some error message")
-    end_mock_message()
-
-    message(FATAL_ERROR "some fatal error message")
-    message(ERROR "some other error message")
-  end_mock_message()
-
-  assert(DEFINED WARNING_MESSAGES)
-  assert(WARNING_MESSAGES STREQUAL "some warning message;some other warning message")
-
-  assert(DEFINED ERROR_MESSAGES)
-  assert("${ERROR_MESSAGES}" STREQUAL "some error message")
-
-  assert(DEFINED FATAL_ERROR_MESSAGES)
-  assert("${FATAL_ERROR_MESSAGES}" STREQUAL "some fatal error message")
-endfunction()
-
 cmake_language(CALL "${TEST_COMMAND}")
