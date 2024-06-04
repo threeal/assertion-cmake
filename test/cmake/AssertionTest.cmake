1b954ffb4fce0f4ada8670f95c0e622f9e257b85
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-04 17:13:25 +0700

    test: move fatal error assertions tests to `AssertFatalError.cmake`

--- test/cmake/AssertionTest.cmake
+++ test/cmake/AssertionTest.cmake
@@ -2,38 +2,6 @@ cmake_minimum_required(VERSION 3.5)
 
 include(Assertion)
 
-function(call_sample_messages)
-  message(WARNING "some warning message")
-  message(WARNING "some other warning message")
-  message(ERROR "some error message")
-  message(FATAL_ERROR "some fatal error message")
-  message(ERROR "some other error message")
-endfunction()
-
-function("Fatal error assertions")
-  function(throw_fatal_error MESSAGE)
-    message(FATAL_ERROR "${MESSAGE}")
-  endfunction()
-
-  assert_fatal_error(
-    CALL throw_fatal_error "some fatal error message"
-    MESSAGE "some fatal error message")
-
-  macro(assert_fail)
-    assert_fatal_error(
-      CALL throw_fatal_error "some fatal error message"
-      MESSAGE "some other fatal error message")
-  endmacro()
-
-  string(
-    JOIN "\n" EXPECTED_MESSAGE
-    "expected fatal error message:"
-    "  some fatal error message"
-    "to be equal to:"
-    "  some other fatal error message")
-  assert_fatal_error(CALL assert_fail MESSAGE "${EXPECTED_MESSAGE}")
-endfunction()
-
 function("Process execution assertions")
   assert_execute_process(COMMAND "${CMAKE_COMMAND}" -E true)
   assert_execute_process(COMMAND "${CMAKE_COMMAND}" -E false ERROR .*)
