73c9cc392259f2b7304e392b533ced3427b82e37
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-21 15:45:25 +0700

    test: add test to check message command after mocked

--- test/AssertFatalError.cmake
+++ test/AssertFatalError.cmake
@@ -2,22 +2,30 @@ cmake_minimum_required(VERSION 3.5)
 
 find_package(Assertion REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 
-function(throw_fatal_error MESSAGE)
-  message(FATAL_ERROR "${MESSAGE}")
-endfunction()
+section("fatal error assertions")
+  function(throw_fatal_error MESSAGE)
+    message(FATAL_ERROR "${MESSAGE}")
+  endfunction()
 
-assert_fatal_error(
-  CALL throw_fatal_error "some fatal error message"
-  MESSAGE "some fatal error message")
-
-macro(assert_fail)
   assert_fatal_error(
     CALL throw_fatal_error "some fatal error message"
-    MESSAGE "some other fatal error message")
-endmacro()
+    MESSAGE "some fatal error message")
 
-string(
-  JOIN "\n" EXPECTED_MESSAGE
-  "expected fatal error message:\n  some fatal error message"
-  "to be equal to:\n  some other fatal error message")
-assert_fatal_error(CALL assert_fail MESSAGE "${EXPECTED_MESSAGE}")
+  macro(assert_fail)
+    assert_fatal_error(
+      CALL throw_fatal_error "some fatal error message"
+      MESSAGE "some other fatal error message")
+  endmacro()
+
+  string(
+    JOIN "\n" EXPECTED_MESSAGE
+    "expected fatal error message:\n  some fatal error message"
+    "to be equal to:\n  some other fatal error message")
+  assert_fatal_error(CALL assert_fail MESSAGE "${EXPECTED_MESSAGE}")
+endsection()
+
+section("mocked message check")
+  message("some unspecified message")
+  message(STATUS "some status message")
+  message(STATUS "some status message" " with additional information")
+endsection()
