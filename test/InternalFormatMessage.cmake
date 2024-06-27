9a23c25b99b2c2f742b13fa2634eb4bde2b3dbcb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-27 16:21:33 +0700

    feat: modify `_assert_internal_format_message` to auto insert `:`

--- test/InternalFormatMessage.cmake
+++ test/InternalFormatMessage.cmake
@@ -3,14 +3,14 @@ cmake_minimum_required(VERSION 3.5)
 find_package(Assertion REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 
 _assert_internal_format_message(
-  MESSAGE "first line" "second line" "third line\nthird line" "fourth line\nfourth line")
+  MESSAGE "first line" "second line"
+    "third line\nthird line" "fourth line\nfourth line")
 
-string(
-  JOIN "\n" EXPECTED_MESSAGE
-  "first line"
-  "  second line"
-  "third line"
-  "third line"
-  "  fourth line"
-  "  fourth line")
-assert(MESSAGE STREQUAL EXPECTED_MESSAGE)
+assert_fatal_error(
+  CALL message FATAL_ERROR "${MESSAGE}"
+  MESSAGE "first line:\n"
+    "  second line\n"
+    "third line\n"
+    "third line:\n"
+    "  fourth line\n"
+    "  fourth line")
