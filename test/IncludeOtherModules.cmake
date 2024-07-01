d4dedb9dccfb2e98a20e3693b272174a6065c88d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-01 21:21:28 +0700

    feat: add `ASSERTION_LIST_FILE` variable

--- test/IncludeOtherModules.cmake
+++ test/IncludeOtherModules.cmake
@@ -3,22 +3,19 @@ section("it should include modules")
   file(WRITE goo.cmake "message(STATUS \"goo\")\n")
 
   assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}"
-      -P ${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake
+    COMMAND "${CMAKE_COMMAND}" -P "${ASSERTION_LIST_FILE}"
       -- foo.cmake goo.cmake
     OUTPUT ".*foo\n.*goo")
 endsection()
 
 section("it should not include any modules if arguments are not specified")
   assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}"
-      -P ${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake
+    COMMAND "${CMAKE_COMMAND}" -P "${ASSERTION_LIST_FILE}"
     OUTPUT "^$")
 endsection()
 
 section("it should not include any modules if included by another module")
-  file(WRITE root.cmake
-    "include(${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake)\n")
+  file(WRITE root.cmake "include(\"${ASSERTION_LIST_FILE}\")\n")
 
   assert_execute_process(
     COMMAND "${CMAKE_COMMAND}" -P root.cmake -- foo.cmake goo.cmake
