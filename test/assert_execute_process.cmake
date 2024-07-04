f132d73720390adb97fe426a1484ce2d213274ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-04 17:24:01 +0700

    test: organize sections in `test/assert_execute_process.cmake`

--- test/assert_execute_process.cmake
+++ test/assert_execute_process.cmake
@@ -1,55 +1,73 @@
-section("execute process assertions")
-  file(TOUCH some-file)
+section("process execution assertions")
+  section("it should assert a process execution")
+    assert_execute_process("${CMAKE_COMMAND}" -E true)
+  endsection()
 
-  assert_execute_process("${CMAKE_COMMAND}" -E true)
-
-  assert_execute_process(
-    "${CMAKE_COMMAND}" -E make_directory some-file ERROR .*)
-
-  assert_fatal_error(
-    CALL assert_execute_process "${CMAKE_COMMAND}" -E true ERROR .*
-    MESSAGE "expected command:\n  ${CMAKE_COMMAND} -E true\nto fail")
-
-  assert_fatal_error(
-    CALL assert_execute_process "${CMAKE_COMMAND}" -E make_directory some-file
-    MESSAGE "expected command:\n"
-      "  ${CMAKE_COMMAND} -E make_directory some-file\n"
-      "not to fail with error:\n"
-      "  Error creating directory \"some-file\".")
+  section("it should fail to assert a process execution")
+    assert_fatal_error(
+      CALL assert_execute_process "${CMAKE_COMMAND}" -E true ERROR .*
+      MESSAGE "expected command:\n  ${CMAKE_COMMAND} -E true\nto fail")
+  endsection()
 endsection()
 
-section("execute process output assertions")
-  assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}" -E echo "Hello world!"
-    OUTPUT "Hello" ".*!")
+section("failed process execution assertions")
+  file(TOUCH some-file)
 
-  assert_fatal_error(
-    CALL assert_execute_process
+  section("it should assert a failed process execution")
+    assert_execute_process(
+      "${CMAKE_COMMAND}" -E make_directory some-file ERROR .*)
+  endsection()
+
+  section("it should fail to assert a failed process execution")
+    assert_fatal_error(
+      CALL assert_execute_process "${CMAKE_COMMAND}" -E make_directory some-file
+      MESSAGE "expected command:\n"
+        "  ${CMAKE_COMMAND} -E make_directory some-file\n"
+        "not to fail with error:\n"
+        "  Error creating directory \"some-file\".")
+  endsection()
+endsection()
+
+section("process execution output assertions")
+  section("it should assert a process execution output")
+    assert_execute_process(
       COMMAND "${CMAKE_COMMAND}" -E echo "Hello world!"
-      OUTPUT "Hello" ".*earth!"
-    MESSAGE "expected the output:\n"
-      ".*\n"
-      "of command:\n"
-      "  ${CMAKE_COMMAND} -E echo Hello world!\n"
-      "to match:\n"
-      "  Hello.*earth!")
+      OUTPUT "Hello" ".*!")
+  endsection()
+
+  section("it should fail to assert a process execution output")
+    assert_fatal_error(
+      CALL assert_execute_process
+        COMMAND "${CMAKE_COMMAND}" -E echo "Hello world!"
+        OUTPUT "Hello" ".*earth!"
+      MESSAGE "expected the output:\n"
+        ".*\n"
+        "of command:\n"
+        "  ${CMAKE_COMMAND} -E echo Hello world!\n"
+        "to match:\n"
+        "  Hello.*earth!")
+  endsection()
 endsection()
 
-section("execute process error assertions")
+section("process execution error assertions")
   file(TOUCH some-file)
 
-  assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}" -E make_directory some-file
-    ERROR "Error creating directory" ".*some-file")
-
-  assert_fatal_error(
-    CALL assert_execute_process
+  section("it should assert a process execution error")
+    assert_execute_process(
       COMMAND "${CMAKE_COMMAND}" -E make_directory some-file
-      ERROR "Error creating directory" ".*some-other-file"
-    MESSAGE "expected the error:\n"
-      ".*\n"
-      "of command:\n"
-      "  ${CMAKE_COMMAND} -E make_directory some-file\n"
-      "to match:\n"
-      "  Error creating directory.*some-other-file")
+      ERROR "Error creating directory" ".*some-file")
+  endsection()
+
+  section("it should fail to assert a process execution error")
+    assert_fatal_error(
+      CALL assert_execute_process
+        COMMAND "${CMAKE_COMMAND}" -E make_directory some-file
+        ERROR "Error creating directory" ".*some-other-file"
+      MESSAGE "expected the error:\n"
+        ".*\n"
+        "of command:\n"
+        "  ${CMAKE_COMMAND} -E make_directory some-file\n"
+        "to match:\n"
+        "  Error creating directory.*some-other-file")
+  endsection()
 endsection()
