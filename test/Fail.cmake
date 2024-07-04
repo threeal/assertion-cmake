9101ed5a9fd141fdf084137f686cb9f55a90d010
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-04 16:15:37 +0700

    feat: modify `fail` function to auto insert `:` (#141)

--- test/Fail.cmake
+++ test/Fail.cmake
@@ -18,30 +18,30 @@ section("it should fail with a formatted fatal error message "
   set(REASON_MULTILINE "some reason\non multiple lines")
 
   assert_fatal_error(
-    CALL fail "something happened:" REASON
+    CALL fail "something happened" REASON
     MESSAGE "something happened:\n  some reason")
 
   assert_fatal_error(
-    CALL fail "something happened:" REASON_MULTILINE
+    CALL fail "something happened" REASON_MULTILINE
     MESSAGE "something happened:\n  some reason\n  on multiple lines")
 
   assert_fatal_error(
-    CALL fail "something happened\non multiple lines:" REASON
+    CALL fail "something happened\non multiple lines" REASON
     MESSAGE "something happened\non multiple lines:\n  some reason")
 
   assert_fatal_error(
-    CALL fail "something happened\non multiple lines:" REASON_MULTILINE
+    CALL fail "something happened\non multiple lines" REASON_MULTILINE
     MESSAGE "something happened\non multiple lines:\n"
       "  some reason\n  on multiple lines")
 
   assert_fatal_error(
-    CALL fail "something happened" "on multiple lines:" REASON
+    CALL fail "something happened" "on multiple lines" REASON
     MESSAGE "something happened\non multiple lines:\n  some reason")
 
   assert_fatal_error(
     CALL fail
-      "something happened" "on multiple lines:" REASON
-      "something else happened:" REASON_MULTILINE
+      "something happened" "on multiple lines" REASON
+      "something else happened" REASON_MULTILINE
     MESSAGE "something happened\non multiple lines:\n  some reason\n"
       "something else happened:\n  some reason\n  on multiple lines")
 endsection()
