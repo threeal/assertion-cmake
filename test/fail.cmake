0f6674633d2b03ea38876d62112dbc4d02520c91
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-09 17:55:31 +0700

    feat: allow fail function to handle consecutive variables (#193)
    * test: organize `fail.cmake` test
    
    * feat: add support in `fail` to handle consecutive variables
    
    * feat: handle consecutive variables if first lines are variable in `fail`

--- test/fail.cmake
+++ test/fail.cmake
@@ -1,47 +1,74 @@
-section("it should fail with a formatted fatal error message")
-  assert_fatal_error(
-    CALL fail "something happened"
-    MESSAGE "something happened")
+section("given strings")
+  section("it should fail with a formatted fatal error message")
+    assert_fatal_error(
+      CALL fail "single line string"
+      MESSAGE "single line string")
 
-  assert_fatal_error(
-    CALL fail "something happened\non multiple lines"
-    MESSAGE "something happened\non multiple lines")
+    assert_fatal_error(
+      CALL fail "multiple\nlines\nstring"
+      MESSAGE "multiple\nlines\nstring")
 
-  assert_fatal_error(
-    CALL fail "something happened" "on multiple lines"
-    MESSAGE "something happened\non multiple lines")
+    assert_fatal_error(
+      CALL fail "single line string" "multiple\nlines\nstring"
+      MESSAGE "single line string\nmultiple\nlines\nstring")
+  endsection()
 endsection()
 
-section("it should fail with a formatted fatal error message "
-  "that contains values")
-  set(REASON "some reason")
-  set(REASON_MULTILINE "some reason\non multiple lines")
+set(SINGLE "single line variable")
+set(MULTIPLE "multiple\nlines\nvariable")
 
-  assert_fatal_error(
-    CALL fail "something happened" REASON
-    MESSAGE "something happened:\n  some reason")
+section("given variables")
+  section("it should fail with a formatted fatal error message")
+    assert_fatal_error(
+      CALL fail SINGLE
+      MESSAGE "single line variable")
 
-  assert_fatal_error(
-    CALL fail "something happened" REASON_MULTILINE
-    MESSAGE "something happened:\n  some reason\n  on multiple lines")
+    assert_fatal_error(
+      CALL fail MULTIPLE
+      MESSAGE "multiple\nlines\nvariable")
 
-  assert_fatal_error(
-    CALL fail "something happened\non multiple lines" REASON
-    MESSAGE "something happened\non multiple lines:\n  some reason")
+    assert_fatal_error(
+      CALL fail SINGLE MULTIPLE
+      MESSAGE "single line variable\nmultiple\nlines\nvariable")
+  endsection()
+endsection()
 
-  assert_fatal_error(
-    CALL fail "something happened\non multiple lines" REASON_MULTILINE
-    MESSAGE "something happened\non multiple lines:\n"
-      "  some reason\n  on multiple lines")
+section("given strings and variables")
+  section("it should fail with a formatted fatal error message")
+    assert_fatal_error(
+      CALL fail "single line string" SINGLE
+      MESSAGE "single line string:\n  single line variable")
 
-  assert_fatal_error(
-    CALL fail "something happened" "on multiple lines" REASON
-    MESSAGE "something happened\non multiple lines:\n  some reason")
+    assert_fatal_error(
+      CALL fail SINGLE "single line string"
+      MESSAGE "single line variable\nsingle line string")
 
-  assert_fatal_error(
-    CALL fail
-      "something happened" "on multiple lines" REASON
-      "something else happened" REASON_MULTILINE
-    MESSAGE "something happened\non multiple lines:\n  some reason\n"
-      "something else happened:\n  some reason\n  on multiple lines")
+    assert_fatal_error(
+      CALL fail "multiple\nlines\nstring" MULTIPLE
+      MESSAGE "multiple\nlines\nstring:\n  multiple\n  lines\n  variable")
+
+    assert_fatal_error(
+      CALL fail MULTIPLE "multiple\nlines\nstring"
+      MESSAGE "multiple\nlines\nvariable\nmultiple\nlines\nstring")
+
+    assert_fatal_error(
+      CALL fail "single line string" "multiple\nlines\nstring" SINGLE MULTIPLE
+      MESSAGE "single line string\nmultiple\nlines\nstring:\n"
+        "  single line variable\n  multiple\n  lines\n  variable")
+
+    assert_fatal_error(
+      CALL fail SINGLE MULTIPLE "single line string" "multiple\nlines\nstring"
+      MESSAGE "single line variable\nmultiple\nlines\nvariable\n"
+        "single line string\nmultiple\nlines\nstring")
+
+    assert_fatal_error(
+      CALL fail "single line string" SINGLE "multiple\nlines\nstring" MULTIPLE
+      MESSAGE "single line string:\n  single line variable\n"
+        "multiple\nlines\nstring:\n  multiple\n  lines\n  variable")
+
+    assert_fatal_error(
+      CALL fail SINGLE "single line string" MULTIPLE "multiple\nlines\nstring"
+      MESSAGE "single line variable\nsingle line string:\n"
+        "  multiple\n  lines\n  variable\nmultiple\nlines\nstring")
+  endsection()
 endsection()
