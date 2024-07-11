3b6e49fa1005227e7c88294722627beacbc7d764
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-11 14:29:42 +0700

    feat: add support for formatting version equality assertion errors (#209)
    * feat: add support to format assertion errors using `VERSION_LESS` operator
    
    * feat: add support to format assertion errors using `VERSION_GREATER` operator
    
    * feat: add support to format assertion errors using `VERSION_EQUAL` operator
    
    * feat: add support to format assertion errors using `VERSION_LESS_EQUAL` operator
    
    * feat: add support to format assertion errors using `VERSION_GREATER_EQUAL` operator

--- test/assert.cmake
+++ test/assert.cmake
@@ -673,6 +673,132 @@ section("string equality condition assertions")
   endsection()
 endsection()
 
+section("version equality condition assertions")
+  set(VERSION_VAR 1.2.3)
+  set(VERSIONN_VAR 1.02.3)
+  set(OTHER_VERSION_VAR 1.3.4)
+
+  section("given equal versions")
+    section("it should assert version equality conditions")
+      assert(NOT 1.2.3 VERSION_LESS 1.02.3)
+      assert(NOT 1.2.3 VERSION_GREATER 1.02.3)
+      assert(1.2.3 VERSION_EQUAL 1.02.3)
+      assert(1.2.3 VERSION_LESS_EQUAL 1.02.3)
+      assert(1.2.3 VERSION_GREATER_EQUAL 1.02.3)
+
+      assert(NOT VERSION_VAR VERSION_LESS VERSIONN_VAR)
+      assert(NOT VERSION_VAR VERSION_GREATER VERSIONN_VAR)
+      assert(VERSION_VAR VERSION_EQUAL VERSIONN_VAR)
+      assert(VERSION_VAR VERSION_LESS_EQUAL VERSIONN_VAR)
+      assert(VERSION_VAR VERSION_GREATER_EQUAL VERSIONN_VAR)
+    endsection()
+
+    section("it should fail to assert version equality conditions")
+      assert_fatal_error(
+        CALL assert 1.2.3 VERSION_LESS 1.02.3
+        MESSAGE "expected version:\n  1.2.3\nto be less than:\n  1.02.3")
+
+      assert_fatal_error(
+        CALL assert 1.2.3 VERSION_GREATER 1.02.3
+        MESSAGE "expected version:\n  1.2.3\nto be greater than:\n  1.02.3")
+
+      assert_fatal_error(
+        CALL assert NOT 1.2.3 VERSION_EQUAL 1.02.3
+        MESSAGE "expected version:\n  1.2.3\nnot to be equal to:\n  1.02.3")
+
+      assert_fatal_error(
+        CALL assert NOT 1.2.3 VERSION_LESS_EQUAL 1.02.3
+        MESSAGE "expected version:\n  1.2.3\n"
+          "not to be less than or equal to:\n  1.02.3")
+
+      assert_fatal_error(
+        CALL assert NOT 1.2.3 VERSION_GREATER_EQUAL 1.02.3
+        MESSAGE "expected version:\n  1.2.3\n"
+          "not to be greater than or equal to:\n  1.02.3")
+    endsection()
+  endsection()
+
+  section("given a lesser version")
+    section("it should assert version equality conditions")
+      assert(1.2.3 VERSION_LESS 1.3.4)
+      assert(NOT 1.2.3 VERSION_GREATER 1.3.4)
+      assert(NOT 1.2.3 VERSION_EQUAL 1.3.4)
+      assert(1.2.3 VERSION_LESS_EQUAL 1.3.4)
+      assert(NOT 1.2.3 VERSION_GREATER_EQUAL 1.3.4)
+
+      assert(VERSION_VAR VERSION_LESS OTHER_VERSION_VAR)
+      assert(NOT VERSION_VAR VERSION_GREATER OTHER_VERSION_VAR)
+      assert(NOT VERSION_VAR VERSION_EQUAL OTHER_VERSION_VAR)
+      assert(VERSION_VAR VERSION_LESS_EQUAL OTHER_VERSION_VAR)
+      assert(NOT VERSION_VAR VERSION_GREATER_EQUAL OTHER_VERSION_VAR)
+    endsection()
+
+    section("it should fail to assert version equality conditions")
+      assert_fatal_error(
+        CALL assert NOT 1.2.3 VERSION_LESS 1.3.4
+        MESSAGE "expected version:\n  1.2.3\nnot to be less than:\n  1.3.4")
+
+      assert_fatal_error(
+        CALL assert 1.2.3 VERSION_GREATER 1.3.4
+        MESSAGE "expected version:\n  1.2.3\nto be greater than:\n  1.3.4")
+
+      assert_fatal_error(
+        CALL assert 1.2.3 VERSION_EQUAL 1.3.4
+        MESSAGE "expected version:\n  1.2.3\nto be equal to:\n  1.3.4")
+
+      assert_fatal_error(
+        CALL assert NOT 1.2.3 VERSION_LESS_EQUAL 1.3.4
+        MESSAGE "expected version:\n  1.2.3\n"
+          "not to be less than or equal to:\n  1.3.4")
+
+      assert_fatal_error(
+        CALL assert 1.2.3 VERSION_GREATER_EQUAL 1.3.4
+        MESSAGE "expected version:\n  1.2.3\n"
+          "to be greater than or equal to:\n  1.3.4")
+    endsection()
+  endsection()
+
+  section("given a greater version")
+    section("it should assert version equality conditions")
+      assert(NOT 1.3.4 VERSION_LESS 1.2.3)
+      assert(1.3.4 VERSION_GREATER 1.2.3)
+      assert(NOT 1.3.4 VERSION_EQUAL 1.2.3)
+      assert(NOT 1.3.4 VERSION_LESS_EQUAL 1.2.3)
+      assert(1.3.4 VERSION_GREATER_EQUAL 1.2.3)
+
+      assert(NOT OTHER_VERSION_VAR VERSION_LESS VERSION_VAR)
+      assert(OTHER_VERSION_VAR VERSION_GREATER VERSION_VAR)
+      assert(NOT OTHER_VERSION_VAR VERSION_EQUAL VERSION_VAR)
+      assert(NOT OTHER_VERSION_VAR VERSION_LESS_EQUAL VERSION_VAR)
+      assert(OTHER_VERSION_VAR VERSION_GREATER_EQUAL VERSION_VAR)
+    endsection()
+
+    section("it should fail to assert version equality conditions")
+      assert_fatal_error(
+        CALL assert 1.3.4 VERSION_LESS 1.2.3
+        MESSAGE "expected version:\n  1.3.4\nto be less than:\n  1.2.3")
+
+      assert_fatal_error(
+        CALL assert NOT 1.3.4 VERSION_GREATER 1.2.3
+        MESSAGE "expected version:\n  1.3.4\nnot to be greater than:\n  1.2.3")
+
+      assert_fatal_error(
+        CALL assert 1.3.4 VERSION_EQUAL 1.2.3
+        MESSAGE "expected version:\n  1.3.4\nto be equal to:\n  1.2.3")
+
+      assert_fatal_error(
+        CALL assert 1.3.4 VERSION_LESS_EQUAL 1.2.3
+        MESSAGE "expected version:\n  1.3.4\n"
+          "to be less than or equal to:\n  1.2.3")
+
+      assert_fatal_error(
+        CALL assert NOT 1.3.4 VERSION_GREATER_EQUAL 1.2.3
+        MESSAGE "expected version:\n  1.3.4\n"
+          "not to be greater than or equal to:\n  1.2.3")
+    endsection()
+  endsection()
+endsection()
+
 section("path equality condition assertions")
   section("it should assert path equality conditions")
     set(PATH_VAR "/some/path")
