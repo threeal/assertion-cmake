b025c75c63803b1a9cf665be2638c5338c57f60d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-06 21:59:56 +0700

    feat: remove automatic policy set from assert function (#244)
    * feat: remove automatic policy set in `assert` function
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test: specify minimum required CMake version in each test script
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * chore: bump minimum required CMake version to 3.24
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- test/assert_fatal_error.cmake
+++ test/assert_fatal_error.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.24)
+
 include(${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake)
 
 section("it should assert a fatal error message")
--- test/fail.cmake
+++ test/fail.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.24)
+
 include(${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake)
 
 section("given strings")
