dee4088a385e70d21e2577ed74028d5a87af57b1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-19 22:06:39 +0700

    test: separate `test_assert.cmake` into several files

---
+++ test/test_assert_number.cmake
@@ -0,0 +1,160 @@
+cmake_minimum_required(VERSION 3.24)
+
+include(${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake)
+
+set(SEVEN_VAR 7)
+set(THIRTEEN_VAR 13)
+
+section("given equal numbers")
+  section("it should assert number equality conditions")
+    assert(NOT 7 LESS 7)
+    assert(NOT 7 GREATER 7)
+    assert(7 EQUAL 7)
+    assert(7 LESS_EQUAL 7)
+    assert(7 GREATER_EQUAL 7)
+
+    assert(NOT SEVEN_VAR LESS SEVEN_VAR)
+    assert(NOT SEVEN_VAR GREATER SEVEN_VAR)
+    assert(SEVEN_VAR EQUAL SEVEN_VAR)
+    assert(SEVEN_VAR LESS_EQUAL SEVEN_VAR)
+    assert(SEVEN_VAR GREATER_EQUAL SEVEN_VAR)
+  endsection()
+
+  section("it should fail to assert number equality conditions")
+    assert_call(assert 7 LESS 7
+      EXPECT_FATAL_ERROR STREQUAL
+        "expected number:\n  7\nto be less than:\n  7")
+
+    assert_call(assert 7 GREATER 7
+      EXPECT_FATAL_ERROR STREQUAL
+        "expected number:\n  7\nto be greater than:\n  7")
+
+    assert_call(assert NOT 7 EQUAL 7
+      EXPECT_FATAL_ERROR STREQUAL
+        "expected number:\n  7\nnot to be equal to:\n  7")
+
+    assert_call(assert NOT 7 LESS_EQUAL 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "not to be less than or equal to:\n  7")
+
+    assert_call(assert NOT 7 GREATER_EQUAL 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "not to be greater than or equal to:\n  7")
+  endsection()
+endsection()
+
+section("given a lesser number")
+  section("it should assert number equality conditions")
+    assert(7 LESS 13)
+    assert(NOT 7 GREATER 13)
+    assert(NOT 7 EQUAL 13)
+    assert(7 LESS_EQUAL 13)
+    assert(NOT 7 GREATER_EQUAL 13)
+
+    assert(SEVEN_VAR LESS THIRTEEN_VAR)
+    assert(NOT SEVEN_VAR GREATER THIRTEEN_VAR)
+    assert(NOT SEVEN_VAR EQUAL THIRTEEN_VAR)
+    assert(SEVEN_VAR LESS_EQUAL THIRTEEN_VAR)
+    assert(NOT SEVEN_VAR GREATER_EQUAL THIRTEEN_VAR)
+  endsection()
+
+  section("it should fail to assert number equality conditions")
+    assert_call(assert NOT 7 LESS 13
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "not to be less than:\n  13")
+
+    assert_call(assert 7 GREATER 13
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be greater than:\n  13")
+
+    assert_call(assert 7 EQUAL 13
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be equal to:\n  13")
+
+    assert_call(assert NOT 7 LESS_EQUAL 13
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "not to be less than or equal to:\n  13")
+
+    assert_call(assert 7 GREATER_EQUAL 13
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be greater than or equal to:\n  13")
+  endsection()
+endsection()
+
+section("given a greater number")
+  section("it should assert number equality conditions")
+    assert(NOT 13 LESS 7)
+    assert(13 GREATER 7)
+    assert(NOT 13 EQUAL 7)
+    assert(NOT 13 LESS_EQUAL 7)
+    assert(13 GREATER_EQUAL 7)
+
+    assert(NOT THIRTEEN_VAR LESS SEVEN_VAR)
+    assert(THIRTEEN_VAR GREATER SEVEN_VAR)
+    assert(NOT THIRTEEN_VAR EQUAL SEVEN_VAR)
+    assert(NOT THIRTEEN_VAR LESS_EQUAL SEVEN_VAR)
+    assert(THIRTEEN_VAR GREATER_EQUAL SEVEN_VAR)
+  endsection()
+
+  section("it should fail to assert number equality conditions")
+    assert_call(assert 13 LESS 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  13\n"
+        "to be less than:\n  7")
+
+    assert_call(assert NOT 13 GREATER 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  13\n"
+        "not to be greater than:\n  7")
+
+    assert_call(assert 13 EQUAL 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  13\n"
+        "to be equal to:\n  7")
+
+    assert_call(assert 13 LESS_EQUAL 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  13\n"
+        "to be less than or equal to:\n  7")
+
+    assert_call(assert NOT 13 GREATER_EQUAL 7
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  13\n"
+        "not to be greater than or equal to:\n  7")
+  endsection()
+endsection()
+
+section("given a non-number")
+  section("it should assert number equality conditions")
+    set(STRING_VAR "some string")
+
+    assert(NOT 7 LESS "some string")
+    assert(NOT 7 GREATER "some string")
+    assert(NOT 7 EQUAL "some string")
+    assert(NOT 7 LESS_EQUAL "some string")
+    assert(NOT 7 GREATER_EQUAL "some string")
+
+    assert(NOT SEVEN_VAR LESS STRING_VAR)
+    assert(NOT SEVEN_VAR GREATER STRING_VAR)
+    assert(NOT SEVEN_VAR EQUAL STRING_VAR)
+    assert(NOT SEVEN_VAR LESS_EQUAL STRING_VAR)
+    assert(NOT SEVEN_VAR GREATER_EQUAL STRING_VAR)
+  endsection()
+
+  section("it should fail to assert number equality conditions")
+    assert_call(assert 7 LESS  "some string"
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be less than:\n  some string")
+
+    assert_call(assert 7 GREATER "some string"
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be greater than:\n  some string")
+
+    assert_call(assert 7 EQUAL "some string"
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be equal to:\n  some string")
+
+    assert_call(assert 7 LESS_EQUAL "some string"
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be less than or equal to:\n  some string")
+
+    assert_call(assert 7 GREATER_EQUAL "some string"
+      EXPECT_FATAL_ERROR STREQUAL "expected number:\n  7\n"
+        "to be greater than or equal to:\n  some string")
+  endsection()
+endsection()
---
+++ test/test_assert_version.cmake
@@ -0,0 +1,121 @@
+cmake_minimum_required(VERSION 3.24)
+
+include(${CMAKE_CURRENT_LIST_DIR}/../cmake/Assertion.cmake)
+
+set(VERSION_VAR 1.2.3)
+set(VERSIONN_VAR 1.02.3)
+set(OTHER_VERSION_VAR 1.3.4)
+
+section("given equal versions")
+  section("it should assert version equality conditions")
+    assert(NOT 1.2.3 VERSION_LESS 1.02.3)
+    assert(NOT 1.2.3 VERSION_GREATER 1.02.3)
+    assert(1.2.3 VERSION_EQUAL 1.02.3)
+    assert(1.2.3 VERSION_LESS_EQUAL 1.02.3)
+    assert(1.2.3 VERSION_GREATER_EQUAL 1.02.3)
+
+    assert(NOT VERSION_VAR VERSION_LESS VERSIONN_VAR)
+    assert(NOT VERSION_VAR VERSION_GREATER VERSIONN_VAR)
+    assert(VERSION_VAR VERSION_EQUAL VERSIONN_VAR)
+    assert(VERSION_VAR VERSION_LESS_EQUAL VERSIONN_VAR)
+    assert(VERSION_VAR VERSION_GREATER_EQUAL VERSIONN_VAR)
+  endsection()
+
+  section("it should fail to assert version equality conditions")
+    assert_call(assert 1.2.3 VERSION_LESS 1.02.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "to be less than:\n  1.02.3")
+
+    assert_call(assert 1.2.3 VERSION_GREATER 1.02.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "to be greater than:\n  1.02.3")
+
+    assert_call(assert NOT 1.2.3 VERSION_EQUAL 1.02.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "not to be equal to:\n  1.02.3")
+
+    assert_call(assert NOT 1.2.3 VERSION_LESS_EQUAL 1.02.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "not to be less than or equal to:\n  1.02.3")
+
+    assert_call(assert NOT 1.2.3 VERSION_GREATER_EQUAL 1.02.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "not to be greater than or equal to:\n  1.02.3")
+  endsection()
+endsection()
+
+section("given a lesser version")
+  section("it should assert version equality conditions")
+    assert(1.2.3 VERSION_LESS 1.3.4)
+    assert(NOT 1.2.3 VERSION_GREATER 1.3.4)
+    assert(NOT 1.2.3 VERSION_EQUAL 1.3.4)
+    assert(1.2.3 VERSION_LESS_EQUAL 1.3.4)
+    assert(NOT 1.2.3 VERSION_GREATER_EQUAL 1.3.4)
+
+    assert(VERSION_VAR VERSION_LESS OTHER_VERSION_VAR)
+    assert(NOT VERSION_VAR VERSION_GREATER OTHER_VERSION_VAR)
+    assert(NOT VERSION_VAR VERSION_EQUAL OTHER_VERSION_VAR)
+    assert(VERSION_VAR VERSION_LESS_EQUAL OTHER_VERSION_VAR)
+    assert(NOT VERSION_VAR VERSION_GREATER_EQUAL OTHER_VERSION_VAR)
+  endsection()
+
+  section("it should fail to assert version equality conditions")
+    assert_call(assert NOT 1.2.3 VERSION_LESS 1.3.4
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "not to be less than:\n  1.3.4")
+
+    assert_call(assert 1.2.3 VERSION_GREATER 1.3.4
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "to be greater than:\n  1.3.4")
+
+    assert_call(assert 1.2.3 VERSION_EQUAL 1.3.4
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "to be equal to:\n  1.3.4")
+
+    assert_call(assert NOT 1.2.3 VERSION_LESS_EQUAL 1.3.4
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "not to be less than or equal to:\n  1.3.4")
+
+    assert_call(assert 1.2.3 VERSION_GREATER_EQUAL 1.3.4
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.2.3\n"
+        "to be greater than or equal to:\n  1.3.4")
+  endsection()
+endsection()
+
+section("given a greater version")
+  section("it should assert version equality conditions")
+    assert(NOT 1.3.4 VERSION_LESS 1.2.3)
+    assert(1.3.4 VERSION_GREATER 1.2.3)
+    assert(NOT 1.3.4 VERSION_EQUAL 1.2.3)
+    assert(NOT 1.3.4 VERSION_LESS_EQUAL 1.2.3)
+    assert(1.3.4 VERSION_GREATER_EQUAL 1.2.3)
+
+    assert(NOT OTHER_VERSION_VAR VERSION_LESS VERSION_VAR)
+    assert(OTHER_VERSION_VAR VERSION_GREATER VERSION_VAR)
+    assert(NOT OTHER_VERSION_VAR VERSION_EQUAL VERSION_VAR)
+    assert(NOT OTHER_VERSION_VAR VERSION_LESS_EQUAL VERSION_VAR)
+    assert(OTHER_VERSION_VAR VERSION_GREATER_EQUAL VERSION_VAR)
+  endsection()
+
+  section("it should fail to assert version equality conditions")
+    assert_call(assert 1.3.4 VERSION_LESS 1.2.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.3.4\n"
+        "to be less than:\n  1.2.3")
+
+    assert_call(assert NOT 1.3.4 VERSION_GREATER 1.2.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.3.4\n"
+        "not to be greater than:\n  1.2.3")
+
+    assert_call(assert 1.3.4 VERSION_EQUAL 1.2.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.3.4\n"
+        "to be equal to:\n  1.2.3")
+
+    assert_call(assert 1.3.4 VERSION_LESS_EQUAL 1.2.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.3.4\n"
+        "to be less than or equal to:\n  1.2.3")
+
+    assert_call(assert NOT 1.3.4 VERSION_GREATER_EQUAL 1.2.3
+      EXPECT_FATAL_ERROR STREQUAL "expected version:\n  1.3.4\n"
+        "not to be greater than or equal to:\n  1.2.3")
+  endsection()
+endsection()
